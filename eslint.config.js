c25baf832504c13261679f907df5186e428c5682
Lok Lok <yellowhappy831@gmail.com>
2026-02-18 16:25:39 +0800

    save

--- eslint.config.js
+++ eslint.config.js
@@ -22,6 +22,8 @@ export default defineConfig([
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
       "react-refresh/only-export-components": "off",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "off"
     }
   },
 ])
